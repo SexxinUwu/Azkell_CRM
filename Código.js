53dec31ee471dfb46c38fad3229596182ce349f7
SexxinUwu <Teffavila2001@gmail.com>
2026-06-06 12:08:13 -0500

    feat: add mobile-responsive inspection dashboard and rename application to Azkell Fleet

--- Código.js
+++ Código.js
@@ -1,5 +1,5 @@
 function doGet() {
-  return HtmlService.createTemplateFromFile('Index').evaluate().setTitle('Azkell CRM').addMetaTag('viewport', 'width=device-width, initial-scale=1').setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+  return HtmlService.createTemplateFromFile('Index').evaluate().setTitle('Azkell Fleet').addMetaTag('viewport', 'width=device-width, initial-scale=1').setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
 function include(filename) { return HtmlService.createHtmlOutputFromFile(filename).getContent(); }
 
